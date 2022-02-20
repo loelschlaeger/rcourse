6d7bc54095250164526ad8c9d047070e37f9b35f
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-02-20 19:35:57 +0100

    Added start-up message.

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+#' @noRd
+#' @importFrom utils packageVersion
+
+.onAttach <- function(lib, pkg) {
+  # startup message
+  msg <- c("Welcome to this tiny course on ggplot2!\n
+           Type\n
+           - slides() to access the slides,\n
+           - tutorial() to start the tutorial,\n
+           - issue() to submit an issue.")
+  packageStartupMessage(msg)
+  invisible()
+}
