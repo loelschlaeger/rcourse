88a0a70aeecc30fefe51813a5a4a31f007825dd4
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2022-02-20 18:42:04 +0000

    Styled package :pencil:

--- R/zzz.R
+++ R/zzz.R
@@ -3,11 +3,13 @@
 
 .onAttach <- function(lib, pkg) {
   # startup message
-  msg <- paste0("Welcome to this tiny course on ggplot2!\n",
-                "Type\n",
-                "- slides() to access the slides,\n",
-                "- tutorial() to start the tutorial,\n",
-                "- issue() to submit an issue.")
+  msg <- paste0(
+    "Welcome to this tiny course on ggplot2!\n",
+    "Type\n",
+    "- slides() to access the slides,\n",
+    "- tutorial() to start the tutorial,\n",
+    "- issue() to submit an issue."
+  )
   packageStartupMessage(msg)
   invisible()
 }
