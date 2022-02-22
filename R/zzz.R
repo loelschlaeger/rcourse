f9bdbac6a7f39bae2eb8a33d4e2228a1b4519a33
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-02-22 16:10:43 +0100

    Added startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -4,11 +4,10 @@
 .onAttach <- function(lib, pkg) {
   # startup message
   msg <- paste0(
-    "Welcome to this tiny course on ggplot2!\n",
-    "Type\n",
-    "- slides() to access the slides,\n",
-    "- tutorial() to start the tutorial,\n",
-    "- issue() to submit an issue."
+    "Welcome to this tiny course on {ggplot2}! Type:\n",
+    "* slides() to access the slides,\n",
+    "* tutorial() to start the tutorial,\n",
+    "* issue() to complain."
   )
   packageStartupMessage(msg)
   invisible()
