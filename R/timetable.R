0dc576b7a364e33827bdb31e612a9598afb8f8be
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2022-03-15 14:31:35 +0000

    Styled package :pencil:

--- R/timetable.R
+++ R/timetable.R
@@ -5,10 +5,12 @@
 
 timetable <- function() {
   data.frame(
-     topic = c("Why and what", "Our first plot", "Break",
-               "Facets and more geoms","Annotations", "Draw Maps", 
-               "Final discussion"),
-     practicals = c("", "yes", "", "yes", "yes", "yes", ""),
-     row.names = c("09 - 10","10 - 12","12 - 13","13 - 14","14 - 15","15 - 16", "16")
+    topic = c(
+      "Why and what", "Our first plot", "Break",
+      "Facets and more geoms", "Annotations", "Draw Maps",
+      "Final discussion"
+    ),
+    practicals = c("", "yes", "", "yes", "yes", "yes", ""),
+    row.names = c("09 - 10", "10 - 12", "12 - 13", "13 - 14", "14 - 15", "15 - 16", "16")
   )
 }
