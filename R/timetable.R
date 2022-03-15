1f7d8e3b3f0cf28e807276b5c6dd225472313dd1
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-03-15 15:27:58 +0100

    Added timetable function.

---
+++ R/timetable.R
@@ -0,0 +1,14 @@
+#' Timetable for the *how_to_ggplot2*-course
+#'
+#' @return
+#' A data frame.
+
+timetable <- function() {
+  data.frame(
+     topic = c("Why and what", "Our first plot", "Break",
+               "Facets and more geoms","Annotations", "Draw Maps", 
+               "Final discussion"),
+     practicals = c("", "yes", "", "yes", "yes", "yes", ""),
+     row.names = c("09 - 10","10 - 12","12 - 13","13 - 14","14 - 15","15 - 16", "16")
+  )
+}
